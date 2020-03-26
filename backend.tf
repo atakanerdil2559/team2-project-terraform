c992b341fa4940f40db99dc223790a617a54f447
mrvgms <mgumuscibik@gmail.com>
2020-03-26 01:22:57 +0000

    changes made

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-project-backend-2020-team2"
-key = "tower/us-east-1/tools/N.Virginia/tower.tfstate"
+key = "tower/us-east-1/tools/n.virginia/tower.tfstate"
 region = "us-east-1"
 dynamodb_table = "terraform-team2"
   }
