5d599f6a65083727be8ca2b2bab47b6ffe12a0a9
mrvgms <mgumuscibik@gmail.com>
2020-03-26 01:15:08 +0000

    +

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ backend "s3" {
 bucket = "terraform-project-backend-2020-team2"
 key = "tower/us-east-1/tools/N.Virginia/tower.tfstate"
 region = "us-east-1"
+dynamodb_table = "terraform-team2"
   }
 }
