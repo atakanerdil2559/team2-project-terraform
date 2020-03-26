53b6017b1fcbed651f58b4cb708227e81bd40838
Cloud User <centos@ip-172-31-41-79.ec2.internal>
2020-03-26 04:40:23 +0000

    created

--- data_source.tf
+++ data_source.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "mysql" {
   backend = "s3"
   config = {
     bucket = "terraform-project-backend-2020"
-    key    = "tower/us-east-1/tools/virginia/tower.tfstate"
+    key    = "team2/us-east-1/tools/virginia/team2.tfstate"
     region = "us-east-1"
   }
 }
