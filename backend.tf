f93175256d2bf787db0ecb9c315cf7974117355f
mrvgms <mgumuscibik@gmail.com>
2020-03-26 03:11:46 +0000

    edited setenv file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-project-backend-2020-team2" 
-    key = "tower/us-east-1/tools/Oregon/tower.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-team2"
+backend "s3" {
+bucket = "terraform-project-backend-2020-team2"
+key = "tower/us-east-1/tools/Ohio/tower.tfstate"
+region = "us-east-1"
+dynamodb_table = "terraform-team2"
   }
 }
--- setenv.sh
+++ setenv.sh
@@ -76,6 +76,7 @@ backend "s3" {
 bucket = "${S3BUCKET}"
 key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}"
 region = "${S3BUCKETREGION}"
+dynamodb_table = "terraform-team2"
   }
 }
 EOF
