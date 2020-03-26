33525b2bf5d3768af1258a2f03ba7fc4d1de439e
Cloud User <centos@ip-172-31-41-79.ec2.internal>
2020-03-26 05:00:06 +0000

    created

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ backend "s3" {
 bucket = "terraform-project-backend-2020-team2"
 key = "team2/us-east-1/tools/virginia/team2.tfstate"
 region = "us-east-1"
-dynamodb_table = "terraform-team2"
+
   }
 }
