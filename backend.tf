63dc16ee881981701159954cdc55dae0ec7379eb
safag05 <safagumus05@gmail.com>
2020-03-26 02:47:11 +0000

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
-backend "s3" {
-bucket = "terraform-project-backend-2020-team2"
-key = "tower/us-east-1/tools/Oregon/tower.tfstate"
-region = "us-east-1"
+  backend "s3" {
+    bucket = "terraform-project-backend-2020-team2" 
+    key = "tower/us-east-1/tools/Oregon/tower.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-team2"
   }
 }
