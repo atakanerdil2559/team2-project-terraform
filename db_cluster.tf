395a4a70406524f465d1a6344c207dfdda0094b8
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-03-28 10:37:42 -0500

    Update db_cluster.tf

--- db_cluster.tf
+++ db_cluster.tf
@@ -20,7 +20,7 @@ resource "aws_rds_cluster" "mydb" {
   backup_retention_period    = "${var.backup_retention_period}"
   skip_final_snapshot        = "${var.skip_final_snapshot}"
   preferred_backup_window    = "${var.preferred_backup_window}"
-  port = "${var.port}"
+#   port = "${var.port}"
   vpc_security_group_ids = ["${data.terraform_remote_state.mysql.sec_group_1}"]
   db_subnet_group_name = "${aws_db_subnet_group.default.name}"
 }
